4427cdf6a1d97a8dcd9c0979d80c8be57f7cdcb9
Kwanghoon Son <kwangson@yahoo.com>
2025-12-12 11:30:13 +0900

    Add model
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- bio/models.py
+++ bio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Person(models.Model):
+    """인물 정보 모델"""
+
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to="persons/", null=True, blank=True)
+    biography = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class LifeEvent(models.Model):
+    """인물의 생애 사건/이정표 모델"""
+
+    person = models.ForeignKey(
+        Person, on_delete=models.CASCADE, related_name="life_events"
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    event_date = models.DateField()
