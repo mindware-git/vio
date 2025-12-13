eb2c15695e1dc899d3389ddc4c31243a38562dd1
Kwanghoon Son <kwangson@yahoo.com>
2025-12-13 21:24:34 +0900

    Add fixture
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from bio import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path("explore/", views.explore, name="explore"),
     path("trending/", views.trending, name="trending"),
     path("bio/<slug:slug>/", include("bio.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
