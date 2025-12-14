0f04c46a336ce621272dfc51c2cca80b7648c558
Kwanghoon Son <kwangson@yahoo.com>
2025-12-14 16:39:57 +0900

    Add i8n
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -102,13 +103,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [
+    ("en", "English"),
+    ("ko", "Korean"),
+    ("zh-hans", "Simplified Chinese"),
+    ("ja", "Japanese"),
+]
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
 
 
 # Static files (CSS, JavaScript, Images)
--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from bio import views
 
+# These URLs will not have a language prefix
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),  # Added for set_language view
+]
+
+# These URLs will have a language prefix (e.g., /en/explore/)
+urlpatterns += i18n_patterns(
     path("", views.home, name="home"),
     path("explore/", views.explore, name="explore"),
     path("trending/", views.trending, name="trending"),
     path("bio/<str:slug>/", include("bio.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+# Media files serving for development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
